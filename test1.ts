d02213488c98b1c194f01f8adab2515294500fdd
CODOmicrobit <50570424+CODOmicrobit@users.noreply.github.com>
2020-02-04 13:56:15 +0100

    Delete test1.ts

--- test1.ts
+++
@@ -1,6 +0,0 @@
-//% Test function for i2c grove color sensor v1.3 or v2.0
-//% It send color value over usb serial
-basic.forever(function () {
-    serial.writeLine("Red:" + Grove_Color_Sensor.get_color(Color.Red) + " - Blue:" + Grove_Color_Sensor.get_color(Color.Blue) + "- Green:" + Grove_Color_Sensor.get_color(Color.Green) + "- Clear:" + Grove_Color_Sensor.get_color(Color.Clear))
-    control.waitMicros(500000)
-})
